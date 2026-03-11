0797b71da85145c15bc7a0b790780522f8717543
ShamuoonHaider <shamuoonh08@gmail.com>
2026-03-11 11:13:42 +0500

    created the number guessing game following the rust book

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,31 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess: String = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small, Try again!"),
+            Ordering::Greater => println!("Too big, Try again!"),
+            Ordering::Equal => {
+                println!("You win! The secret number was {}", secret_number);
+                break;
+            }
+        }
+    }
+}
--- rust_project_1/src/main.rs
+++ rust_project_1/src/main.rs
@@ -1,3 +1,13 @@
+// fn main() {
+//     let num: &str = "5";
+//     if num == "5" {
+//         println!("{}", num);
+//     } else {
+//         println!("num is not 5");
+//     }
+// }
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello world!")
 }
